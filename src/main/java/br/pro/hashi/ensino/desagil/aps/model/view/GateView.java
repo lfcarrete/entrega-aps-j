8f451ef26d38d8d8954c985f1a51013e10bcabf4
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2020-05-17 16:43:03 -0300

    "Add initial files"

---
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/model/view/GateView.java
@@ -0,0 +1,164 @@
+package br.pro.hashi.ensino.desagil.aps.model.view;
+
+import br.pro.hashi.ensino.desagil.aps.model.model.Gate;
+import br.pro.hashi.ensino.desagil.aps.model.model.Light;
+import br.pro.hashi.ensino.desagil.aps.model.model.Switch;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.net.URL;
+
+public class GateView extends FixedPanel implements ActionListener, MouseListener {
+    private final Gate gate;
+
+    private final Light light;
+
+    private final JCheckBox inA;
+    private final JCheckBox inB;
+    private final JCheckBox out;
+
+    private final Switch s0;
+    private final Switch s1;
+
+    private final Image image;
+
+    private Color color;
+
+    public GateView(Gate gate) {
+        super(320, 200);
+
+        this.gate = gate;
+
+        inA = new JCheckBox();
+        inB = new JCheckBox();
+        out = new JCheckBox();
+
+        s0 = new Switch();
+        s1 = new Switch();
+
+        light = new Light(255, 0, 0);
+
+        if (gate.getInputSize() != 1) {
+
+            add(inA, 50, 60, 20, 20);
+            add(inB, 50, 107, 20, 20);
+
+
+            inA.addActionListener(this);
+            inB.addActionListener(this);
+        } else {
+            add(inA, 50, 77, 20, 20);
+            inA.addActionListener(this);
+        }
+
+        color = Color.BLACK;
+
+        String name = gate.toString() + ".png";
+        URL url = getClass().getClassLoader().getResource(name);
+        image = getToolkit().getImage(url);
+
+        light.connect(0, gate);
+
+        addMouseListener(this);
+
+        out.setEnabled(false);
+
+        update();
+    }
+
+    private void update() {
+        color = light.getColor();
+        repaint();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        boolean inAState = inA.isSelected();
+        boolean inBState = inB.isSelected();
+
+        if (inAState) {
+            s0.turnOn();
+        } else {
+            s0.turnOff();
+        }
+        if (inBState) {
+            s1.turnOn();
+        } else {
+            s1.turnOff();
+        }
+        if (gate.getInputSize() != 1) {
+            gate.connect(0, s0);
+            gate.connect(1, s1);
+        } else {
+            gate.connect(0, s0);
+        }
+        update();
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent event) {
+        boolean result = gate.read();
+        // Descobre em qual posição o clique ocorreu.
+        int x = event.getX();
+        int y = event.getY();
+
+        double m = Math.pow((Math.pow((x - 230), 2) + Math.pow((y - 79), 2)), 0.5);
+        // Se o clique foi dentro do quadrado colorido...
+        if (m < 25) {
+            if (result) {
+                // ...então abrimos a janela seletora de cor...
+                color = JColorChooser.showDialog(this, null, color);
+
+                light.setColor(color);
+
+                // ...e chamamos repaint para atualizar a tela.
+                repaint();
+            }
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent mouseEvent) {
+
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+
+        // Não podemos esquecer desta linha, pois não somos os
+        // únicos responsáveis por desenhar o painel, como era
+        // o caso nos Desafios. Agora é preciso desenhar também
+        // componentes internas, e isso é feito pela superclasse.
+        super.paintComponent(g);
+
+        // Desenha a imagem, passando sua posição e seu tamanho.
+        g.drawImage(image, 20, 0, 250, 200, this);
+
+        // Desenha um quadrado cheio.
+        g.setColor(color);
+        g.fillOval(230, 79, 25, 25);
+
+        // Linha necessária para evitar atrasos
+        // de renderização em sistemas Linux.
+        getToolkit().sync();
+    }
+}
